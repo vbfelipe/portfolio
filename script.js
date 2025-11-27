d232a9978e20c8a6fe0495d06a1a3f409c7846e1
Felipe Vicentini Braga <felipe.braga@km.srv.br>
2025-11-27 18:32:23 -0300

    JS-only file added

---
+++ script.js
@@ -0,0 +1,22 @@
+
+    const contactBtn = document.getElementById("contactBtn");
+    const contactForm = document.getElementById("contactForm");
+    const closeForm = document.getElementById("closeForm");
+    const messageForm = document.getElementById("messageForm");
+
+    contactBtn.addEventListener("click", () => {
+      contactBtn.style.display = "none";
+      contactForm.style.display = "flex";
+    });
+
+    closeForm.addEventListener("click", () => {
+      contactForm.style.display = "none";
+      contactBtn.style.display = "inline-block";
+    });
+
+    /* Forced redirect fix after submit */
+    messageForm.addEventListener("submit", () => {
+      setTimeout(() => {
+        window.location.href = "https://vbfelipe.github.io/portfolio/";
+      }, 1500);
+    });
